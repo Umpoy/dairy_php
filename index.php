f5cdbbe11c745eb6b9e8c75e5b5d33334a4bb47f
Umpoy <iumpoy@yahoo.com>
2018-07-06 16:31:19 -0700

    form is now connected to phpmyadmin

--- index.php
+++ index.php
@@ -1,19 +1,33 @@
 <?php
-    if(array_key_exist("submit", $_POST)){
+    if(array_key_exists("submit", $_POST)){
+        $link = mysqli_connect("localhost", "root", "root", "dairy"); // connects to phpmyadmin
         $error = "";
-        if(!$_POST['email']){ // checks if email input is not filled in
+        if(!$_POST['email']){ // if email is not inputed
             $error .= "An email address is required";
         }
-        if(!$_POST['password']){ // check if password input is not filled in
+        if(!$_POST['password']){ // if password is not inputed
             $error .= "A password is required";
         }
-        if($error != ""){ // if error is not complete
+        if($error != ""){ // if email or password is not are not filled in
             $error = "<p>There were error(s) in your form:</p>".$error;
-        } else { // everything is okay
-
+        } else { // email and password was entered
+            $query = "SELECT id FROM `users` WHERE email = '".mysqli_real_escape_string($link, $_POST['email'])."' LIMIT 1";
+            $result = mysqli_query($link, $query);
+            if(mysqli_num_rows($result) > 0){
+                $error = "That email address is taken";
+            } else {
+                $query = "INSERT INTO `users` (`email`, `password`) VALUES ('".mysqli_real_escape_string($link, $_POST['email'])."', '".mysqli_real_escape_string($link, $_POST['password'])."')";
+                if(!mysqli_query($link, $query)){
+                    $error = "<p>Could not sign you up - please try again later.</p>";
+                } else {
+                    echo "Sign up successful";
+                }
+            }
         }
     }
 ?>
+
+<div id="error"><?php echo $error; ?></div>
 
 <form method="post">
     <input type="email" name="email" placeholder="Your Email">
