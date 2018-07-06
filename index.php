e80e92421a1a67061b9056fafcb011f207f3148e
Umpoy <iumpoy@yahoo.com>
2018-07-06 16:12:44 -0700

    added basic form

---
+++ index.php
@@ -0,0 +1,5 @@
+<form method="post">
+<input type="email" name="email" placeholder="Your Email">
+<input type="password" name="password" placeholder="Password">
+<input type="checkbox" name="stayLoggedIn" value=1>
+</form>
