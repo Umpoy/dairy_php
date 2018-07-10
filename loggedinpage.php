90907c6405a67bd3461e36fd1b30063d97e1c566
Umpoy <iumpoy@yahoo.com>
2018-07-10 14:22:08 -0700

    textarea now sends and receives data from database

--- loggedinpage.php
+++ loggedinpage.php
@@ -1,10 +1,15 @@
 <?php
     session_start();
+    $diaryContent = "";
     if(array_key_exists("id", $_COOKIE)){
         $_SESSION['id'] = $_COOKIE['id'];
     }
     if(array_key_exists("id", $_SESSION)){
         echo "Logged In! <a href='index.php?logout=1'>Log out</a>";
+        include("connect.php");
+        $query = "SELECT diary FROM `users` WHERE id = ".mysqli_real_escape_string($link, $_SESSION['id'])." LIMIT 1";
+        $row = mysqli_fetch_array(mysqli_query($link, $query));
+        $diaryContent = $row['diary'];
     } else {
         header("Location: index.php");
     }
@@ -24,7 +29,7 @@
     </head>
     <body>
         <div class="container">
-                <textarea id="diary"></textarea>
+                <textarea id="diary"><?php echo $diaryContent ?></textarea>
         </div>
 
         <!-- Optional JavaScript -->
