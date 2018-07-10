a2c1c2b598f22bdd854b32ad07bff9aeb3f8b168
Umpoy <iumpoy@yahoo.com>
2018-07-10 08:06:37 -0700

    ajax call to updataDatabase.php

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,15 @@
 $(".toggleForms").on("click", function () {
     $("#signUpForm").toggle();
     $("#logInForm").toggle();
+});
+
+$("#dairy").bind("input propertychange", function () {
+    $.ajax({
+        method: "POST",
+        url: "updateDatabase.php",
+        data: { content: $("#dairy").val() }
+    })
+        .done(function (msg) {
+            alert("Data Saved: " + msg);
+        });
 })
--- loggedinpage.php
+++ loggedinpage.php
@@ -6,9 +6,6 @@
     }
     if(array_key_exists("id", $_SESSION)){
         echo "Logged In! <a href='index.php?logout=1'>Log out</a>";
-        $query = "SELECT id FROM `users` WHERE email = '".mysqli_real_escape_string($link, $_POST['email'])."' LIMIT 1";
-        $result = mysqli_query($link, $query);
-        echo $result;
     } else {
         header("Location: index.php");
     }
@@ -33,7 +30,7 @@
 
         <!-- Optional JavaScript -->
         <!-- jQuery first, then Popper.js, then Bootstrap JS -->
-        <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+        <script src="https://code.jquery.com/jquery-3.1.1.min.js">
         <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
         <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
         <script src="assets/js/main.js"></script>
